9891814ee6f782831e36230ce1e215975ecc90ab
Vishal1061 <vishal.bisht1061@gmail.com>
2021-03-29 00:08:29 +0530

    commit

--- ForLoopStatement/src/com/company/Main.java
+++ ForLoopStatement/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        for (int i = 2; i<9;i++){
+            System.out.println("10,000 at " + i + "% " + "interest = " + calculateInterest(10000.0,i));
+        }
+    }
+    public static double calculateInterest(double amount, double interestRate){
+        return (amount *(interestRate/100));
     }
 }
