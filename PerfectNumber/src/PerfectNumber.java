401d44f315b06f106b8777f2eb0823922ad473b9
Vishal1061 <vishal.bisht1061@gmail.com>
2021-03-29 22:56:03 +0530

    commit

---
+++ PerfectNumber/src/PerfectNumber.java
@@ -0,0 +1,23 @@
+public class PerfectNumber {
+
+    public static boolean isPerfectNumber(int number){
+
+        int sum = 0;
+
+        if (number<1){
+            return false;
+        } else{
+            for (int i =1; i <number; i ++){
+                if (number % i == 0){
+                    sum+=i;
+                }
+            }
+            if (sum == number){
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+    }
+}
