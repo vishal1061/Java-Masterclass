43091e19ccbef9e2fb991c307837a36ee044b920
Vishal1061 <vishal.bisht1061@gmail.com>
2021-03-29 13:05:35 +0530

    commit

---
+++ WhileDoWhile/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int count = 1;
+        while (count !=6){
+            System.out.println("Count value is " + count);
+            count++;
+        }
+        // Similar for loop.
+        System.out.println("************************************************************");
+        System.out.println("************************************************************");
+        for (count = 1; count!=6;count++){
+
+            System.out.println("Count value is " + count);
+        }
+    }
+}
