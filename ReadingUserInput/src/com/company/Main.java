6c84b45f0fc7ee62e6e95cdad9d835f9096cc4f5
Vishal1061 <vishal.bisht1061@gmail.com>
2021-04-01 01:08:52 +0530

    commit

---
+++ ReadingUserInput/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine(); // Handle next line character(enter key)
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        int age = 2021 -yearOfBirth;
+        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+        scanner.close();
+    }
+}
