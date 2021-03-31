967e927102a5c8775dfc3fe0bf9e1e2f6e2b21de
Vishal1061 <vishal.bisht1061@gmail.com>
2021-04-01 00:18:40 +0530

    commit

--- DiagonalStar/src/DiagonalStar.java
+++ DiagonalStar/src/DiagonalStar.java
@@ -1,2 +1,28 @@
 public class DiagonalStar {
-}
+    public static void printSquareStar(int number){
+        if (number <5 ){
+            System.out.println("Invalid Value");
+        }else{
+            for (int r=1; r<=number; r++){
+                for (int c = 1; c<=number; c++){
+                    if(r==1 || r== number){
+                        System.out.print('*');
+                    }else if(c==1 || c == number){
+                        System.out.print('*');
+                    }else if (r==c){
+                        System.out.print("*");
+                    }else if(c==(number - r + 1)){
+                        System.out.print("*");
+                    }else {
+                        System.out.print(" ");
+                    }
+
+
+
+                    }
+                System.out.print('\n');
+                }
+            }
+        }
+    }
+
